6ed81a33e0a14975c5d426736b6dcd1fdbdda30c
MB6718 <mb6718@mail.ru>
2020-12-14 03:01:36 +0500

    add(main): added copyright label and page tabs.
    - Added a label with copyright on the bottom panel.
    - Added tabs: "main", "device", "application", "about".

--- main.pas
+++ main.pas
@@ -5,10 +5,21 @@ unit main;
 interface
 
 uses
-  Classes, SysUtils, FileUtil, LResources, Forms, Controls, Graphics, Dialogs;
+  Classes, SysUtils, FileUtil, LResources, Forms, Controls, Graphics, Dialogs,
+  ComCtrls, ExtCtrls, StdCtrls;
 
 type
+
+  { TfMain }
+
   TfMain = class(TForm)
+    Label1: TLabel;
+    PageControl1: TPageControl;
+    Panel1: TPanel;
+    TabSheet1: TTabSheet;
+    TabSheet2: TTabSheet;
+    TabSheet3: TTabSheet;
+    TabSheet4: TTabSheet;
   private
 
   public
