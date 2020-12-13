8c2d284aea4415db840599ba0e4922451bd96758
MB6718 <mb6718@mail.ru>
2020-12-14 01:44:49 +0500

    new(project): init "USB WatchDog" project.
    - Created "USB WatchDog utility" project in lazarus IDE.
    - Added the necessary settings to the project.

---
+++ main.pas
@@ -0,0 +1,27 @@
+unit main;
+
+{$mode objfpc}{$H+}
+
+interface
+
+uses
+  Classes, SysUtils, FileUtil, LResources, Forms, Controls, Graphics, Dialogs;
+
+type
+  TfMain = class(TForm)
+  private
+
+  public
+
+  end;
+
+var
+  fMain: TfMain;
+
+implementation
+
+initialization
+  {$R *.lfm}
+
+end.
+
